61b484e186187b892a88e72efefb4d22ca79e050
Nathan Zimmerman <npzimmerman@gmail.com>
2024-03-12 13:38:53 -0500

    Add deps to setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,8 @@ setup(
     name='beam_pyspark_runner',
     version='0.1',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "apache-beam==2.53.0",
+        "pyspark=3.5.1"
+    ],
 )
